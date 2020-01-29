337de7cc7a1772805dd4de22d2f1cc65a33e6707
sean <seanutf@gmail.com>
2020-01-29 22:20:18 +0800

    feature:创建BaseActivity

--- login/src/main/java/com/sean/android/login/LoginActivity.java
+++ login/src/main/java/com/sean/android/login/LoginActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+import com.seanutf.cmmonui.BaseActivity;
+
+public class LoginActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

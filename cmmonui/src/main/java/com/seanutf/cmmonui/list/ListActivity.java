70fb9b1922ca2e15683fe1d697d8a2d92db26039
sean <seanutf@gmail.com>
2020-02-10 00:36:45 +0800

    feature：添加点击事件

--- cmmonui/src/main/java/com/seanutf/cmmonui/list/ListActivity.java
+++ cmmonui/src/main/java/com/seanutf/cmmonui/list/ListActivity.java
@@ -2,15 +2,39 @@ package com.seanutf.cmmonui.list;
 
 
 import android.os.Bundle;
+import android.view.View;
 
-import com.seanutf.cmmonui.R;
 import com.seanutf.cmmonui.arch.BaseActivity;
+import com.seanutf.cmmonui.databinding.ActivityListBinding;
 
 public class ListActivity extends BaseActivity {
 
+    ActivityListBinding layoutBind;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list);
+        layoutBind = ActivityListBinding.inflate(getLayoutInflater());
+        setContentView(layoutBind.getRoot());
+
+        layoutBind.tvSvRv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //todo 处理
+            }
+        });
+
+        layoutBind.tvCroRv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //todo 处理
+            }
+        });
+
+        layoutBind.tvCroSv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //todo 处理
+            }
+        });
     }
 }

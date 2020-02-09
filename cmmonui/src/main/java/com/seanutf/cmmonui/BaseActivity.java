f4183e635fe68262e143ef3a1983f1f18c92d21d
sean <seanutf@gmail.com>
2020-02-09 23:16:24 +0800

    build:进行第三方库升级和build配置：解决：Cannot resolve symbol ViewModelProviders on AppCompatActivity

--- cmmonui/src/main/java/com/seanutf/cmmonui/BaseActivity.java
+++ cmmonui/src/main/java/com/seanutf/cmmonui/BaseActivity.java
@@ -1,6 +1,21 @@
 package com.seanutf.cmmonui;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
 }

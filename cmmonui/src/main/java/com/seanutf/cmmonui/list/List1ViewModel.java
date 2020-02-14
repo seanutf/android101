14e7dc2fad68b06a2f2405715062207e57583d89
sean <seanutf@gmail.com>
2020-02-14 23:54:02 +0800

    feature：viewModel

--- cmmonui/src/main/java/com/seanutf/cmmonui/list/List1ViewModel.java
+++ cmmonui/src/main/java/com/seanutf/cmmonui/list/List1ViewModel.java
@@ -4,5 +4,5 @@ import com.seanutf.cmmonui.arch.BaseViewModel;
 
 public class List1ViewModel extends BaseViewModel {
     // TODO: Implement the ViewModel
-    //todo:测试ViewModel
+    //测试ViewModel
 }

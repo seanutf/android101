95353190a4494ad0246eff1ae6e4664400a73441
zhangyi <zhangyi@bonbonwork.com>
2020-06-08 10:19:49 +0800

    feature:商品调研

--- app/src/main/java/com/seanutf/android/test/Test2Activity.java
+++ app/src/main/java/com/seanutf/android/test/Test2Activity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.MergeAdapter;
 
 import com.seanutf.android.base.databinding.ActivityTest2Binding;
 import com.seanutf.cmmonui.arch.BaseActivity;
@@ -11,6 +12,7 @@ import com.seanutf.cmmonui.arch.BaseActivity;
 public class Test2Activity extends BaseActivity {
 
     ActivityTest2Binding vb;
+    MergeAdapter mergeAdapter;
 
     public static void startActivity(BaseActivity aa) {
         aa.startActivity(new Intent(aa, Test2Activity.class));
@@ -21,5 +23,6 @@ public class Test2Activity extends BaseActivity {
         super.onCreate(savedInstanceState);
         vb = ActivityTest2Binding.inflate(getLayoutInflater());
         setContentView(vb.getRoot());
+        //mergeAdapter.addAdapter();
     }
 }
---
+++ app/src/main/java/com/seanutf/android/test/TestAK.kt
@@ -0,0 +1,59 @@
+package com.seanutf.android.test
+/**
+ * @property name
+ * @constructor gofgokfo
+ * */
+open class TestAK
+(name:Int?){
+
+    /**
+     * @sample
+     * */
+    var aa:Int= 1
+
+    fun main() {
+        println("Hello world!")
+
+        var a = 1
+        // 模板中的简单名称：
+        val s1 = "a is $a"
+
+        a = 2
+        // 模板中的任意表达式：
+        val s2 = "${s1.replace("is", "was")}, but now is $a"
+    }
+
+    /**
+     * @return fugiodfjgoid
+     * @exception e fdidjfiodspf
+     * @throws NullPointerException hfdsjfsdgksdfg
+     * @see [main]
+     *
+     * */
+    fun sum(a: Int, b: Int): Int {
+        return a + b
+    }
+}
+
+open class TestBD{
+    fun getList(){
+
+    }
+}
+/**
+ * @param a is a num to max
+ *
+ * */
+fun maxOf(a: Int, b: Int): Int {
+    if (a > b) {
+        return a
+    } else {
+        return b
+    }
+}
+
+
+fun main() {
+    println("Hello world!")
+}
+

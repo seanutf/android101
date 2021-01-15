c4ebad5813dce765edefd59c2fd615627b5a0159
sean <seanutf@gmail.com>
2021-01-15 23:49:47 +0800

    feature：添加列表测试

---
+++ app/src/main/java/com/seanutf/android/listtest/ListTestActivity.kt
@@ -0,0 +1,12 @@
+package com.seanutf.android.listtest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.seanutf.android.R
+
+class ListTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_test)
+    }
+}

accec3092b803bb57bcfcbe27294e3cd5af109d3
seanutf <seanutf@gmail.com>
2021-01-19 17:43:58 +0800

    dev:添加参考链接

--- app/src/main/java/com/seanutf/android/main/TodoList.kt
+++ app/src/main/java/com/seanutf/android/main/TodoList.kt
@@ -11,4 +11,16 @@ class TodoList {
      * https://blog.csdn.net/CSDN2497242041/article/details/79294470
      * http://www.voidcn.com/article/p-uqxygtyd-dk.html
      * */
+    /**
+     * todo 2 列表埋点需求
+     * 需求：滑入滑出的展示和时长，以及页面级别的跳转前后台切换时的展示和不展示
+     * 预想实现1 滑动监听
+     * 预想实现2 Item数据级别记录
+     * 预想实现3 airbnb/epoxy
+     * 预想实现4 paging 3
+     * 参考：https://github.com/airbnb/epoxy
+     * https://medium.com/airbnb-engineering/epoxy-airbnbs-view-architecture-on-android-c3e1af150394#.9hno4vdc0
+     * https://my.oschina.net/u/4365870/blog/4268093
+     * https://cloud.tencent.com/developer/article/1667330
+     * */
 }

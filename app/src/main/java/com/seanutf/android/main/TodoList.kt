ba1ac0fff6070d4734455528c2d6bf3fbc5b9a41
seanutf <seanutf@gmail.com>
2021-01-19 17:35:24 +0800

    dev:添加todo

---
+++ app/src/main/java/com/seanutf/android/main/TodoList.kt
@@ -0,0 +1,12 @@
+package com.seanutf.android.main
+
+/**
+ * @Author zhangyi
+ * @Date 2021/1/19 5:33 PM
+ * @Desc 一个todo列表，当闲下来时提醒我调研哪些东西
+ */
+class TodoList {
+    /**
+     * todo 1 列表刷新后浏览位置的恢复
+     * */
+}
